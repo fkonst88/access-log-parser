57ff458ea5870aca7136839ec5c3b133898a501b
F_Konst <fkonst88@gmail.com>
2025-10-31 23:36:35 +0300

    add 2 number experiments: addition, subtraction, multiplication, division

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        //Получаем два числа из консоли
+        System.out.println("Введите первое число:");
+        int numberOne = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int numberTwo = new Scanner(System.in).nextInt();
+
+        //Вычисляем сумму двух чисел
+        int addition = numberOne + numberTwo;
+        System.out.println("сумма: " + numberOne + " + " + numberTwo + " = " + addition);
+
+        //Вычисляем разность двух чисел
+        int subtraction = numberOne - numberTwo;
+        System.out.println("разность: " + numberOne + " - " + numberTwo + " = " + subtraction);
+
+        //Вычисляем произведение двух чисел
+        int multiplication = numberOne * numberTwo;
+        System.out.println("произведение: " + numberOne + " * " + numberTwo + " = " + multiplication);
+
+        //Вычисляем частное двух чисел
+        double division = (double)numberOne / numberTwo;
+        System.out.println("частное: " + numberOne + " / " + numberTwo + " = " + division);
     }
 }
