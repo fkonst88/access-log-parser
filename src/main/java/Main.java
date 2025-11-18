dc11feab51f62ebb5b35878414df1f41e119b34f
F_Konst <fkonst88@gmail.com>
2025-11-18 12:16:57 +0300

    check is file exists

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        //бесконечный цикл while, в котором в консоли запрашивается путь к файлу
+        int sumPath = 0;
+        while (true) {
+            System.out.println("Укажите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            if (!fileExists) {
+                System.out.println("Указанный файл не существует");
+                continue;
+            }
+            boolean isDirectory = file.isDirectory();
+            if (!isDirectory) {
+                sumPath++;
+                System.out.println("Путь указан верно. Это файл номер N" + sumPath);
+            } else {
+                System.out.println("Указанный путь является путём к папке");
+            }
+        }
     }
 }
